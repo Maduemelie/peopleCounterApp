0175a052585b0b6c0e36a01f929e74eb3b5cac72
Madu Chiemelie <maduemelie@gmail.com>
2022-09-01 22:04:15 +0100

    added a record button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let count = 0;
 let button = document.querySelector('#increment-btn')
+let counter = document.querySelector(".count-er")
 button.addEventListener("click", increment)
 
 
@@ -9,18 +10,33 @@ button.addEventListener("click", increment)
 function increment(){
     let counter = document.querySelector(".count-er")
     count = count + 1
-    counter.innerText = count //this increases the inner text 0
+    counter.textContent = count //this increases the inner text 0
 }
+const recordSaved = document.querySelector('.recordedscore')
+// console.log(recordSaved)
+record = 0
+
+
+recordSaved.addEventListener('click',() =>{
+    record += count;
+    recordSaved.textContent = recordSaved.textContent + record + ' ' ;
+})
+// const recordsaved = () => {
+
+// }
 
 saveBtn= document.querySelector("#save-btn")
 saveBtn.addEventListener("click", save) // event listener that runs the save function on clicking the save button
 
 let saveEl = document.getElementById('save-el');
-console.log(saveEl)
+// console.log(saveEl)
 
 function save(){
     let countStr = count + " - "; // catenating two strings
-    saveEl.innerText += countStr // updating the innerText using the countStr variable 
+    saveEl.textContent += countStr // updating the innerText using the countStr variable 
+    counter.textContent = 0 
+    count = 0
+    
 }
 
 
